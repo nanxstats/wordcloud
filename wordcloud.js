390aa4141b121c8fff62d89fcc9e0454041e8d9b
Nan Xiao <me@nanx.me>
2023-06-18 02:21:35 -0400

    Add color palette selection support

--- wordcloud.js
+++ wordcloud.js
@@ -1,3 +1,49 @@
+// Define a custom D3.js categorical color palette with the viridis color scheme and supports the .domain method
+let viridis = [
+  "#fde725",
+  "#dde318",
+  "#bade28",
+  "#95d840",
+  "#75d054",
+  "#56c667",
+  "#3dbc74",
+  "#29af7f",
+  "#20a386",
+  "#1f968b",
+  "#238a8d",
+  "#287d8e",
+  "#2d718e",
+  "#33638d",
+  "#39558c",
+  "#404688",
+  "#453781",
+  "#482576",
+  "#481467",
+  "#440154",
+];
+d3.scale.viridis = function () {
+  return d3.scale.ordinal().range(viridis);
+};
+d3.scale.viridisReversed = function () {
+  return d3.scale.ordinal().range(viridis.slice().reverse());
+};
+
+let okabeIto = [
+  "#E69F00",
+  "#56B4E9",
+  "#009E73",
+  "#F0E442",
+  "#0072B2",
+  "#D55E00",
+  "#CC79A7",
+];
+d3.scale.okabeIto = function () {
+  return d3.scale.ordinal().range(okabeIto);
+};
+d3.scale.okabeItoReversed = function () {
+  return d3.scale.ordinal().range(okabeIto.slice().reverse());
+};
+
 function flatten(t, e) {
   if ("string" == typeof t) return t;
   var n = [];
@@ -31,6 +77,10 @@ function generate() {
   layout
     .font(d3.select("#font").property("value"))
     .spiral(d3.select("input[name=spiral]:checked").property("value")),
+    // Get fill color palette from the select input
+    (fill = d3.scale[d3.select("#color-palette").property("value")]().domain([
+      0, 1e6,
+    ])),
     (fontSize = d3.scale[
       d3.select("input[name=scale]:checked").property("value")
     ]().range([10, 100])),
@@ -113,6 +163,7 @@ function draw(t, e) {
         "translate(" + [w >> 1, h >> 1] + ")scale(" + scale + ")"
       );
 }
+
 function downloadPNG() {
   d3.event.preventDefault();
   var t = document.createElement("canvas"),
@@ -475,7 +526,6 @@ function downloadSVG() {
 })("undefined" == typeof exports ? d3.layout || (d3.layout = {}) : exports);
 var unicodePunctuationRe =
     "!-#%-*,-/:;?@\\[-\\]_{}\xa1\xa7\xab\xb6\xb7\xbb\xbf\u037e\u0387\u055a-\u055f\u0589\u058a\u05be\u05c0\u05c3\u05c6\u05f3\u05f4\u0609\u060a\u060c\u060d\u061b\u061e\u061f\u066a-\u066d\u06d4\u0700-\u070d\u07f7-\u07f9\u0830-\u083e\u085e\u0964\u0965\u0970\u0af0\u0df4\u0e4f\u0e5a\u0e5b\u0f04-\u0f12\u0f14\u0f3a-\u0f3d\u0f85\u0fd0-\u0fd4\u0fd9\u0fda\u104a-\u104f\u10fb\u1360-\u1368\u1400\u166d\u166e\u169b\u169c\u16eb-\u16ed\u1735\u1736\u17d4-\u17d6\u17d8-\u17da\u1800-\u180a\u1944\u1945\u1a1e\u1a1f\u1aa0-\u1aa6\u1aa8-\u1aad\u1b5a-\u1b60\u1bfc-\u1bff\u1c3b-\u1c3f\u1c7e\u1c7f\u1cc0-\u1cc7\u1cd3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205e\u207d\u207e\u208d\u208e\u2329\u232a\u2768-\u2775\u27c5\u27c6\u27e6-\u27ef\u2983-\u2998\u29d8-\u29db\u29fc\u29fd\u2cf9-\u2cfc\u2cfe\u2cff\u2d70\u2e00-\u2e2e\u2e30-\u2e3b\u3001-\u3003\u3008-\u3011\u3014-\u301f\u3030\u303d\u30a0\u30fb\ua4fe\ua4ff\ua60d-\ua60f\ua673\ua67e\ua6f2-\ua6f7\ua874-\ua877\ua8ce\ua8cf\ua8f8-\ua8fa\ua92e\ua92f\ua95f\ua9c1-\ua9cd\ua9de\ua9df\uaa5c-\uaa5f\uaade\uaadf\uaaf0\uaaf1\uabeb\ufd3e\ufd3f\ufe10-\ufe19\ufe30-\ufe52\ufe54-\ufe61\ufe63\ufe68\ufe6a\ufe6b\uff01-\uff03\uff05-\uff0a\uff0c-\uff0f\uff1a\uff1b\uff1f\uff20\uff3b-\uff3d\uff3f\uff5b\uff5d\uff5f-\uff65",
-  fill = d3.scale.category20b(),
   w = 960,
   h = 600,
   words = [],
