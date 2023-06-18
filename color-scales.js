d097f455963c3ded16f928fe13dcba29f743dc35
Nan Xiao <me@nanx.me>
2023-06-18 16:43:54 -0400

    Add Gephi color scale

--- color-scales.js
+++ color-scales.js
@@ -46,3 +46,48 @@ d3.scale.okabeIto = function () {
 d3.scale.okabeItoReversed = function () {
   return d3.scale.ordinal().range(okabeIto.slice().reverse());
 };
+
+// Gephi color scale
+let gephi = [
+  "#F1087B",
+  "#5FA5FD",
+  "#06BB1B",
+  "#E72094",
+  "#D3A207",
+  "#9183E2",
+  "#E7663B",
+  "#67A030",
+  "#E558B2",
+  "#F05252",
+  "#62A131",
+  "#8985E1",
+  "#E25BBD",
+  "#D87823",
+  "#C9821C",
+  "#7D77CE",
+  "#69C73F",
+  "#D44443",
+  "#CE51A9",
+  "#5D8D2B",
+  "#D06D20",
+  "#C364D7",
+  "#59BF42",
+  "#D4404A",
+  "#6E7ACA",
+  "#CA4B93",
+  "#7E9321",
+  "#6E7ACA",
+  "#5BC344",
+  "#D64E2B",
+  "#C94992",
+  "#C78A1C",
+  "#C465D6",
+  "#D14058",
+  "#699025",
+];
+d3.scale.gephi = function () {
+  return d3.scale.ordinal().range(gephi);
+};
+d3.scale.gephiReversed = function () {
+  return d3.scale.ordinal().range(gephi.slice().reverse());
+};
